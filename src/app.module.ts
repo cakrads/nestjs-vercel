9e3f6edd4f7d686a77c52ac661fdb77b31f69669
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-01-24 20:32:37 +0700

    add/import PrismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 // import { join } from 'path';
 // import { ServeStaticModule } from '@nestjs/serve-static';
 
+import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './modules/products';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './modules/products';
+
 @Module({
   imports: [
     // ServeStaticModule.forRoot({
     //   rootPath: join(__dirname, '../..', 'public'),
     // }),
     ProductsModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
